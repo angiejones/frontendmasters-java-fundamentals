c4ef937eacab153804d63fa7c5da6414cb86741b
angiejones <jones.angie@gmail.com>
2023-03-15 11:56:23 -0500

    abstraction

---
+++ src/main/java/abstraction/Shape.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public abstract class Shape {
+
+    abstract double calculateArea();
+
+    public void print(){
+        System.out.println("I am a shape");
+    }
+}
---
+++ src/main/java/abstraction/ShapeTester.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public class ShapeTester {
+
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5, 7);
+        rectangle.print();
+        System.out.println(rectangle.calculateArea());
+    }
+}
